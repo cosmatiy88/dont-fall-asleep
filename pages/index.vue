5aa64ba26ce9d1c30ee358bb12f3b2a31a5e47ce
Thomas Chin <tchin25@gmail.com>
2020-10-17 14:49:10 -0400

    Ported opencv into vue
    This is cursed
    
    Co-Authored-By: Andrew <46986624+AndrewCaylor@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,10 @@
 export default {
   data() {
     return {
-      status: "not loaded"
+      status: "not loaded",
+      audio: null,
+      soundPlaying: false,
+      playSound: false
     };
   },
   head() {
@@ -31,7 +34,7 @@ export default {
           async: true,
           body: true,
           callback: () => {
-            onOpenCvReady();
+            this.onOpenCvReady();
             console.log("callback called");
             this.status = "loaded";
           }
@@ -47,6 +50,290 @@ export default {
     },
     stopCv() {
       cvInterval.clearInterval();
+    },
+    onOpenCvReady() {
+      let utils = new this.Utils("errorMessage"); //use utils class
+      let averageEyes = 2;
+      const TRIGGER_AVERAGE = 0.3;
+      this.audio = new Audio("SeinfeldTheme.mp3");
+      let processVideo;
+      let cvInterval;
+
+      cv["onRuntimeInitialized"] = () => {
+        //initializing cv variables
+
+        const video = document.getElementById("webcam");
+
+        const cap = new cv.VideoCapture(video);
+        const src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+
+        const faceCascadeFile = "haarcascade_frontalface_default.xml";
+        const eyeCascadeFile = "haarcascade_eye.xml";
+
+        const gray = new cv.Mat();
+        const faces = new cv.RectVector();
+        const eyes = new cv.RectVector();
+        const faceCascade = new cv.CascadeClassifier();
+        const eyeCascade = new cv.CascadeClassifier();
+
+        const msize = new cv.Size(0, 0);
+
+        navigator.mediaDevices
+          .getUserMedia({ video: true })
+          .then(stream => {
+            video.srcObject = stream;
+
+            // use createFileFromUrl to "pre-build" the xml
+            utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
+              faceCascade.load(faceCascadeFile);
+              utils.createFileFromUrl(eyeCascadeFile, eyeCascadeFile, () => {
+                eyeCascade.load(eyeCascadeFile);
+
+                //process video
+                const FPS = 30;
+
+                processVideo = () => {
+                  // let begin = Date.now();
+                  // start processing.
+
+                  cap.read(src);
+                  cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+
+                  let eyesMax = 0;
+
+                  //running the model to find the face
+                  faceCascade.detectMultiScale(
+                    gray,
+                    faces,
+                    1.1,
+                    3,
+                    0,
+                    msize,
+                    msize
+                  );
+
+                  // Draws rectangles around the face
+                  for (let i = 0; i < faces.size(); ++i) {
+                    let roiGray = gray.roi(faces.get(i));
+                    let roiSrc = src.roi(faces.get(i));
+                    let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
+                    let point2 = new cv.Point(
+                      faces.get(i).x + faces.get(i).width,
+                      faces.get(i).y + faces.get(i).height
+                    );
+                    cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
+
+                    //running the model to detect the eyes
+                    eyeCascade.detectMultiScale(roiGray, eyes);
+
+                    //draw rectangles around the eyes
+                    for (let j = 0; j < eyes.size(); ++j) {
+                      let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
+                      let point2 = new cv.Point(
+                        eyes.get(j).x + eyes.get(j).width,
+                        eyes.get(j).y + eyes.get(j).height
+                      );
+                      cv.rectangle(roiSrc, point1, point2, [0, 0, 255, 255]);
+                    }
+
+                    if (eyesMax < eyes.size()) eyesMax = eyes.size();
+
+                    roiGray.delete();
+                    roiSrc.delete();
+                  }
+
+                  averageEyes = 0.8 * averageEyes + 0.2 * eyesMax;
+
+                  console.log(averageEyes);
+
+                  if (averageEyes < TRIGGER_AVERAGE) {
+                    if (!this.playSound && !this.soundPlaying) {
+                      this.playSound = true;
+
+                      this.startSound();
+                    }
+                  } else {
+                    this.playSound = false;
+                  }
+
+                  // Draws cv output (src) onto id="canvasOutput"
+                  cv.imshow("canvasOutput", src);
+
+                  // let delay = 1000 / FPS - (Date.now() - begin);
+                };
+
+                // Processes video
+                cvInterval = setInterval(processVideo, 40);
+              });
+            });
+          })
+          .catch(function(err0r) {
+            console.log(err0r);
+          });
+      };
+    },
+    Utils(errorOutputId) {
+      // eslint-disable-line no-unused-vars
+      let self = this;
+      this.errorOutput = document.getElementById(errorOutputId);
+
+      const OPENCV_URL = "opencv.js";
+
+      this.loadOpenCv = function(onloadCallback) {
+        let script = document.createElement("script");
+        script.setAttribute("async", "");
+        script.setAttribute("type", "text/javascript");
+        script.addEventListener("load", () => {
+          if (cv.getBuildInformation) {
+            console.log(cv.getBuildInformation());
+            onloadCallback();
+          } else {
+            // WASM
+            cv["onRuntimeInitialized"] = () => {
+              console.log(cv.getBuildInformation());
+              onloadCallback();
+            };
+          }
+        });
+        script.addEventListener("error", () => {
+          self.printError("Failed to load " + OPENCV_URL);
+        });
+        script.src = OPENCV_URL;
+        let node = document.getElementsByTagName("script")[0];
+        node.parentNode.insertBefore(script, node);
+      };
+
+      this.createFileFromUrl = function(path, url, callback) {
+        let request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.responseType = "arraybuffer";
+        request.onload = function(ev) {
+          if (request.readyState === 4) {
+            if (request.status === 200) {
+              let data = new Uint8Array(request.response);
+              cv.FS_createDataFile("/", path, data, true, false, false);
+              callback();
+            } else {
+              self.printError(
+                "Failed to load " + url + " status: " + request.status
+              );
+            }
+          }
+        };
+        request.send();
+      };
+
+      this.loadImageToCanvas = function(url, cavansId) {
+        let canvas = document.getElementById(cavansId);
+        let ctx = canvas.getContext("2d");
+        let img = new Image();
+        img.crossOrigin = "anonymous";
+        img.onload = function() {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0, img.width, img.height);
+        };
+        img.src = url;
+      };
+
+      this.clearError = function() {
+        this.errorOutput.innerHTML = "";
+      };
+
+      this.printError = function(err) {
+        if (typeof err === "undefined") {
+          err = "";
+        } else if (typeof err === "number") {
+          if (!isNaN(err)) {
+            if (typeof cv !== "undefined") {
+              err = "Exception: " + cv.exceptionFromPtr(err).msg;
+            }
+          }
+        } else if (typeof err === "string") {
+          let ptr = Number(err.split(" ")[0]);
+          if (!isNaN(ptr)) {
+            if (typeof cv !== "undefined") {
+              err = "Exception: " + cv.exceptionFromPtr(ptr).msg;
+            }
+          }
+        } else if (err instanceof Error) {
+          err = err.stack.replace(/\n/g, "<br>");
+        }
+        this.errorOutput.innerHTML = err;
+      };
+
+      function onVideoCanPlay() {
+        if (self.onCameraStartedCallback) {
+          self.onCameraStartedCallback(self.stream, self.video);
+        }
+      }
+
+      this.startCamera = function(resolution, callback, videoId) {
+        const constraints = {
+          qvga: { width: { exact: 320 }, height: { exact: 240 } },
+          vga: { width: { exact: 640 }, height: { exact: 480 } }
+        };
+        let video = document.getElementById(videoId);
+        if (!video) {
+          video = document.createElement("video");
+        }
+
+        let videoConstraint = constraints[resolution];
+        if (!videoConstraint) {
+          videoConstraint = true;
+        }
+
+        navigator.mediaDevices
+          .getUserMedia({ video: videoConstraint, audio: false })
+          .then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+            self.video = video;
+            self.stream = stream;
+            self.onCameraStartedCallback = callback;
+            video.addEventListener("canplay", onVideoCanPlay, false);
+          })
+          .catch(function(err) {
+            self.printError("Camera Error: " + err.name + " " + err.message);
+          });
+      };
+
+      this.stopCamera = function() {
+        if (this.video) {
+          this.video.pause();
+          this.video.srcObject = null;
+          this.video.removeEventListener("canplay", onVideoCanPlay);
+        }
+        if (this.stream) {
+          this.stream.getVideoTracks()[0].stop();
+        }
+      };
+    },
+    startSound: async function() {
+      while (this.audio.readyState != 4) {
+        await this.sleep(100);
+      }
+
+      this.audio.play();
+      console.log("playing song");
+      this.soundPlaying = true;
+
+      this.audio.addEventListener("ended", function() {
+        this.audio.currentTime = 0;
+      });
+
+      while (this.playSound) {
+        await this.sleep(50);
+      }
+
+      console.log("pausing");
+
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.soundPlaying = false;
+    },
+    sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
     }
   }
 };
--- static/index.js
+++ static/index.js
@@ -1,162 +1,155 @@
-let utils = new Utils("errorMessage"); //use utils class
-let averageEyes = 2;
-const TRIGGER_AVERAGE = 0.3;
-let playingSound = false;
-let soundPlaying = false;
-let audio = new Audio("SeinfeldTheme.mp3");
-let processVideo;
-let cvInterval;
+// let utils = new Utils("errorMessage"); //use utils class
+// let averageEyes = 2;
+// const TRIGGER_AVERAGE = 0.3;
+// let playingSound = false;
+// let soundPlaying = false;
+// let audio = new Audio("SeinfeldTheme.mp3");
+// let processVideo;
+// let cvInterval;
 
-function onOpenCvReady() {
-    console.log("onOpenCvReady");
-    cv["onRuntimeInitialized"] = () => {
-        //initializing cv variables
+// function onOpenCvReady() {
+//     console.log("onOpenCvReady");
+//     cv["onRuntimeInitialized"] = () => {
+//         //initializing cv variables
 
-        let video = document.getElementById("webcam");
+//         const video = document.getElementById("webcam");
 
 
-        let cap = new cv.VideoCapture(video);
-        let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+//         const cap = new cv.VideoCapture(video);
+//         const src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
 
-        let faceCascadeFile = "haarcascade_frontalface_default.xml";
-        let eyeCascadeFile = "haarcascade_eye.xml";
+//         const faceCascadeFile = "haarcascade_frontalface_default.xml";
+//         const eyeCascadeFile = "haarcascade_eye.xml";
 
-        let gray = new cv.Mat();
-        let faces = new cv.RectVector();
-        let eyes = new cv.RectVector();
-        let faceCascade = new cv.CascadeClassifier();
-        let eyeCascade = new cv.CascadeClassifier();
+//         const gray = new cv.Mat();
+//         const faces = new cv.RectVector();
+//         const eyes = new cv.RectVector();
+//         const faceCascade = new cv.CascadeClassifier();
+//         const eyeCascade = new cv.CascadeClassifier();
 
-        let msize = new cv.Size(0, 0);
+//         const msize = new cv.Size(0, 0);
 
-        navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then(function(stream) {
-                video.srcObject = stream;
+//         navigator.mediaDevices
+//             .getUserMedia({ video: true })
+//             .then(function(stream) {
+//                 video.srcObject = stream;
 
-                // use createFileFromUrl to "pre-build" the xml
-                utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
-                    faceCascade.load(faceCascadeFile);
-                    utils.createFileFromUrl(eyeCascadeFile, eyeCascadeFile, () => {
-                        eyeCascade.load(eyeCascadeFile);
+//                 // use createFileFromUrl to "pre-build" the xml
+//                 utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
+//                     faceCascade.load(faceCascadeFile);
+//                     utils.createFileFromUrl(eyeCascadeFile, eyeCascadeFile, () => {
+//                         eyeCascade.load(eyeCascadeFile);
 
-                        //process video
-                        const FPS = 30;
+//                         //process video
+//                         const FPS = 30;
 
-                        processVideo = () => {
-                            // let begin = Date.now();
-                            // start processing.
+//                         processVideo = () => {
+//                             // let begin = Date.now();
+//                             // start processing.
 
-                            cap.read(src);
-                            cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+//                             cap.read(src);
+//                             cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
 
-                            let eyesMax = 0;
+//                             let eyesMax = 0;
 
-                            //running the model to find the face
-                            faceCascade.detectMultiScale(
-                                gray,
-                                faces,
-                                1.1,
-                                3,
-                                0,
-                                msize,
-                                msize
-                            );
+//                             //running the model to find the face
+//                             faceCascade.detectMultiScale(
+//                                 gray,
+//                                 faces,
+//                                 1.1,
+//                                 3,
+//                                 0,
+//                                 msize,
+//                                 msize
+//                             );
 
-                            // Draws rectangles around the face
-                            for (let i = 0; i < faces.size(); ++i) {
-                                let roiGray = gray.roi(faces.get(i));
-                                let roiSrc = src.roi(faces.get(i));
-                                let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-                                let point2 = new cv.Point(
-                                    faces.get(i).x + faces.get(i).width,
-                                    faces.get(i).y + faces.get(i).height
-                                );
-                                cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
+//                             // Draws rectangles around the face
+//                             for (let i = 0; i < faces.size(); ++i) {
+//                                 let roiGray = gray.roi(faces.get(i));
+//                                 let roiSrc = src.roi(faces.get(i));
+//                                 let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
+//                                 let point2 = new cv.Point(
+//                                     faces.get(i).x + faces.get(i).width,
+//                                     faces.get(i).y + faces.get(i).height
+//                                 );
+//                                 cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
 
-                                //running the model to detect the eyes
-                                eyeCascade.detectMultiScale(roiGray, eyes);
+//                                 //running the model to detect the eyes
+//                                 eyeCascade.detectMultiScale(roiGray, eyes);
 
-                                //draw rectangles around the eyes
-                                for (let j = 0; j < eyes.size(); ++j) {
-                                    let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
-                                    let point2 = new cv.Point(
-                                        eyes.get(j).x + eyes.get(j).width,
-                                        eyes.get(j).y + eyes.get(j).height
-                                    );
-                                    cv.rectangle(roiSrc, point1, point2, [0, 0, 255, 255]);
-                                }
+//                                 //draw rectangles around the eyes
+//                                 for (let j = 0; j < eyes.size(); ++j) {
+//                                     let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
+//                                     let point2 = new cv.Point(
+//                                         eyes.get(j).x + eyes.get(j).width,
+//                                         eyes.get(j).y + eyes.get(j).height
+//                                     );
+//                                     cv.rectangle(roiSrc, point1, point2, [0, 0, 255, 255]);
+//                                 }
 
-                                if (eyesMax < eyes.size()) eyesMax = eyes.size();
+//                                 if (eyesMax < eyes.size()) eyesMax = eyes.size();
 
-                                roiGray.delete();
-                                roiSrc.delete();
-                            }
+//                                 roiGray.delete();
+//                                 roiSrc.delete();
+//                             }
 
-                            averageEyes = 0.8 * averageEyes + 0.2 * eyesMax;
+//                             averageEyes = 0.8 * averageEyes + 0.2 * eyesMax;
 
-                            console.log(averageEyes);
+//                             console.log(averageEyes);
 
-                            if (averageEyes < TRIGGER_AVERAGE) {
-                                if (!playSound && !soundPlaying) {
-                                    playSound = true;
+//                             if (averageEyes < TRIGGER_AVERAGE) {
+//                                 if (!playSound && !soundPlaying) {
+//                                     playSound = true;
 
-                                    startSound();
-                                }
-                            } else {
-                                playSound = false;
-                            }
+//                                     startSound();
+//                                 }
+//                             } else {
+//                                 playSound = false;
+//                             }
 
-                            ////////////////////////////////
+//                             ////////////////////////////////
 
-                            // Draws cv output (src) onto id="canvasOutput"
-                            cv.imshow("canvasOutput", src);
+//                             // Draws cv output (src) onto id="canvasOutput"
+//                             cv.imshow("canvasOutput", src);
 
-                            // schedule the next one.
-                            // let delay = 1000 / FPS - (Date.now() - begin);
-                            // setTimeout(processVideo, delay);
-                        };
+//                             // let delay = 1000 / FPS - (Date.now() - begin);
+//                         };
 
-                        // schedule the first one.
-                        // setTimeout(processVideo, 0);
-                        cvInterval = setInterval(processVideo, 40);
+//                         // Processes video
+//                         cvInterval = setInterval(processVideo, 40);
+//                     });
+//                 });
+//             })
+//             .catch(function(err0r) {
+//                 console.log(err0r);
+//             });
+//     };
+// }
 
-                        // while(notStopped){
-                        //     processVideo()
-                        // }
-                    });
-                });
-            })
-            .catch(function(err0r) {
-                console.log(err0r);
-            });
-    };
-}
+// async function startSound() {
+//     while (audio.readyState != 4) {
+//         await sleep(100);
+//     }
 
-async function startSound() {
-    while (audio.readyState != 4) {
-        await sleep(100);
-    }
+//     audio.play();
+//     console.log("playing song");
+//     soundPlaying = true;
 
-    audio.play();
-    console.log("playing song");
-    soundPlaying = true;
+//     audio.addEventListener("ended", function() {
+//         audio.currentTime = 0;
+//     });
 
-    audio.addEventListener("ended", function() {
-        audio.currentTime = 0;
-    });
+//     while (playSound) {
+//         await sleep(50);
+//     }
 
-    while (playSound) {
-        await sleep(50);
-    }
+//     console.log("pausing");
 
-    console.log("pausing");
+//     audio.pause();
+//     audio.currentTime = 0;
+//     soundPlaying = false;
+// }
 
-    audio.pause();
-    audio.currentTime = 0;
-    soundPlaying = false;
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
